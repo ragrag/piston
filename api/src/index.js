039023c83c1ced731a6f69d69873ade4ccf36d38
Raggi <35541698+ragrag@users.noreply.github.com>
2021-04-10 00:51:26 +0200

    add api key auth

--- api/src/index.js
+++ api/src/index.js
@@ -67,6 +67,13 @@ const app = express();
   app.use(body_parser.urlencoded({ extended: true }));
   app.use(body_parser.json());
 
+  if (process.env.SECURE_WITH_API_KEY === "true")
+    app.use(function (req, res, next) {
+      const apiKey = req.headers[process.env.API_KEY_HEADER];
+      if (apiKey !== process.env.API_KEY) return req.status(401).send();
+      next();
+    });
+
   const validate = (req, res, next) => {
     const errors = validationResult(req);
 
