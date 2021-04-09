c957f2b535a2003fafe1a7c6214704ef05c79aaa
Raggi <35541698+ragrag@users.noreply.github.com>
2021-04-10 01:06:00 +0200

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -67,6 +67,11 @@ const app = express();
   app.use(body_parser.urlencoded({ extended: true }));
   app.use(body_parser.json());
 
+  console.log(
+    process.env.SECURE_WITH_API_KEY,
+    process.env.API_KEY_HEADER,
+    process.env.API_KEY
+  );
   if (process.env.SECURE_WITH_API_KEY === "true")
     app.use(function (req, res, next) {
       const apiKey = req.headers[process.env.API_KEY_HEADER];
