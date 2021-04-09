d460eb0d2bef2f3a49c37deea7e416694e430188
Raggi <35541698+ragrag@users.noreply.github.com>
2021-04-10 01:41:15 +0200

    add api key to cli

--- cli/index.js
+++ cli/index.js
@@ -1,26 +1,28 @@
 #!/usr/bin/env node
+require("dotenv").config();
+const axios = require("axios").default;
 
-const axios = require('axios').default;
+const axiosHeader = {};
+axiosHeader[process.env.API_KEY_HEADER] = process.env.API_KEY;
+const axios_instance = function (argv) {
+  argv.axios = axios.create({
+    baseURL: argv["piston-url"],
+    headers: { ...axiosHeader },
+  });
 
-const axios_instance = function(argv){
-    argv.axios = axios.create({
-        baseURL: argv['piston-url']
-    });
-
-    return argv;
+  return argv;
 };
 
-require('yargs')(process.argv.slice(2))
-    .option('piston-url', {
-        alias: ['u'],
-        default: 'http://127.0.0.1:2000',
-        desc: 'Piston API URL',
-        string: true
-    })
-    .middleware(axios_instance)
-    .scriptName("piston")
-    .commandDir('commands')
-    .demandCommand()
-    .help()
-    .wrap(72)
-    .argv;
+require("yargs")(process.argv.slice(2))
+  .option("piston-url", {
+    alias: ["u"],
+    default: "http://127.0.0.1:2000",
+    desc: "Piston API URL",
+    string: true,
+  })
+  .middleware(axios_instance)
+  .scriptName("piston")
+  .commandDir("commands")
+  .demandCommand()
+  .help()
+  .wrap(72).argv;
