890ed32b56a82ac14e7d6c8ced4722de731b2509
Raggi <35541698+ragrag@users.noreply.github.com>
2021-06-02 17:33:11 +0200

    Update job.js

--- api/src/executor/job.js
+++ api/src/executor/job.js
@@ -284,7 +284,7 @@ class Job {
           .replace(/(\s\r\n|\s\n|\s\r)/gm, "")
           .trim();
 
-        if (run_output !== run_expected_output) {
+        if (run_output.localeCompare(run_expected_output) !== 0) {
           return {
             compile,
             run,
