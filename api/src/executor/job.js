249ef206cf53925405da9d991a0c88c0efd1b0ab
Raggi <35541698+ragrag@users.noreply.github.com>
2021-06-15 03:38:17 +0200

    Update job.js

--- api/src/executor/job.js
+++ api/src/executor/job.js
@@ -270,7 +270,7 @@ class Job {
           result: {
             status: Verdict.TLE,
             stdout: run[i].stdout,
-            stdin: this.stdin[i].replace(/(\r|\n)/gm, "").trim(),
+            stdin: this.stdin[i].replace(/(\r)/gm, "\n").trim(),
             expected_output,
           },
         };
